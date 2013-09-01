bf90b5eaf3ca72b30703db84db28a1e6ab944a82
MrD <the.ubik@gmail.com>
2013-09-01 18:35:38 +0300

    Check the network class - No route to host :(

--- src/gr/uoa/di/monitoring/android/activities/MainActivity.java
+++ src/gr/uoa/di/monitoring/android/activities/MainActivity.java
@@ -10,7 +10,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 
+import com.commonsware.cwac.wakeful.WakefulIntentService;
+
 import gr.uoa.di.monitoring.android.R;
+import gr.uoa.di.monitoring.android.services.NetworkService;
+
+import org.apache.http.conn.util.InetAddressUtils;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class MainActivity extends BaseActivity implements OnClickListener {
@@ -29,6 +39,9 @@ public class MainActivity extends BaseActivity implements OnClickListener {
 		for (int button : BUTTONS) {
 			findViewById(button).setOnClickListener(this);
 		}
+		// 192.168.1.65
+		w("IP : " + getIPAddress(true));
+		WakefulIntentService.sendWakefulWork(this, NetworkService.class);
 		// String msg = null;
 		// if (android.os.Build.VERSION.SDK_INT >=
 		// Build.VERSION_CODES.HONEYCOMB) {
@@ -93,5 +106,40 @@ public class MainActivity extends BaseActivity implements OnClickListener {
 	protected void onPause() {
 		super.onPause();
 		detailsLayout = prefsLayout = UNDEFINED;
+	}
+
+	/**
+	 * Get IP address from first non-localhost interface
+	 *
+	 * @param ipv4
+	 *            true=return ipv4, false=return ipv6
+	 * @return address or empty string
+	 */
+	public static String getIPAddress(boolean useIPv4) {
+		try {
+			List<NetworkInterface> interfaces = Collections
+					.list(NetworkInterface.getNetworkInterfaces());
+			for (NetworkInterface intf : interfaces) {
+				List<InetAddress> addrs = Collections.list(intf
+						.getInetAddresses());
+				for (InetAddress addr : addrs) {
+					if (!addr.isLoopbackAddress()) {
+						String sAddr = addr.getHostAddress().toUpperCase();
+						boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
+						if (useIPv4) {
+							if (isIPv4) return sAddr;
+						} else {
+							if (!isIPv4) {
+								int delim = sAddr.indexOf('%'); // drop ip6 port
+																// suffix
+								return delim < 0 ? sAddr : sAddr.substring(0,
+									delim);
+							}
+						}
+					}
+				}
+			}
+		} catch (Exception ex) {} // for now eat exceptions
+		return "";
 	}
 }
